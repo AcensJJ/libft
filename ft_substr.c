ba031ba77449baac39ecd729abe99db8d6a7edb2
Jean-jacques Acens <jacens@z1r7p2.le-101.fr>
2019-10-24 12:59:03 +0200

    no leak on substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jacens <jacens@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 10:59:20 by jacens       #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/22 19:53:15 by jacens      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/24 12:58:13 by jacens      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,8 +21,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (s == NULL)
 		return (NULL);
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	if (start > ft_strlen(s))
 		return (ft_calloc(1, 1));
 	if (!(ptr = malloc(len + 1)) || s == 0)
